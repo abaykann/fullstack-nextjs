653c65d2238933d3a37940f16d38ceb891edcf5a
abaykann <azizakbaral@gmail.com>
2021-09-16 21:53:34 +0700

    fixed bug decimal precision scale for million price

--- migrations/20210913164524_create_products_table.js
+++ migrations/20210913164524_create_products_table.js
@@ -3,9 +3,9 @@ exports.up = function(knex) {
     return knex.schema.createTable('products', function(table) {
         table.increments();
         table.string('productName').notNullable();
-        table.decimal('price', 8,2).notNullable();
+        table.decimal('price', 12,2).notNullable();
         table.integer('disc').notNullable().defaultTo(0)
-        table.decimal('netPrice', 8,2).notNullable();
+        table.decimal('netPrice', 12,2).notNullable();
         table.integer('categoryId').notNullable();
         table.string('imageUrl').notNullable();
         table.string('pubId').notNullable();
