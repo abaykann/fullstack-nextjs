369d0ee4c55b9873fe295bbf5ea3176c5b7e8186
abaykann <azizakbaral@gmail.com>
2021-09-22 22:36:21 +0700

    update theme

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,32 @@
+// import { createTheme } from '@mui/material/styles';
+// import { red } from '@mui/material/colors';
+
+// // Create a theme instance.
+// const theme = createTheme({
+//   palette: {
+//     primary: {
+//       main: '#ffffff',
+//     },
+//     secondary: {
+//       main: '#4b014b',
+//     },
+//     white: {
+//       main: '#FAFAFA',
+//     },
+//     error: {
+//       main: red.A400,
+//     },
+//     background: {
+//       // default: "#f3f3f4"
+//       default: "#f2f4f7"
+//     }
+//   },
+// });
+
+// export default theme;
+
+
+
 import { createTheme } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
 
@@ -5,13 +34,10 @@ import { red } from '@mui/material/colors';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#ffffff',
+      main: '#556cd6',
     },
     secondary: {
-      main: '#4b014b',
-    },
-    white: {
-      main: '#FAFAFA',
+      main: '#19857b',
     },
     error: {
       main: red.A400,
