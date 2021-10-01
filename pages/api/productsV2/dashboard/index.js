9857b496c5e1b608c213ef0d8842934e7934bd8c
abaykann <azizakbaral@gmail.com>
2021-10-02 01:08:02 +0700

    Create index.js

---
+++ pages/api/productsV2/dashboard/index.js
@@ -0,0 +1,26 @@
+// import db from '../../../libs/db';
+
+// export default async function handler(req, res) {
+//     if(req.method !== 'GET') return res.status(405).end();
+
+
+//     try {
+
+//         const data = await db('products').count()
+
+//         res.status(200);
+//         res.json({
+//             // message: 'Product deleted successfully'
+//             code: 200,
+//             data:data
+//         });
+
+//     }catch (err) {
+//         res.status(500).json({
+//           error: 'something is wrong',
+//           status: 'false',
+//           code: 500
+//         });
+//         console.log(err);
+//       }
+// }
