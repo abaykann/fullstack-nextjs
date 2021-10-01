bd1b69236e0458fa74542a058342463125f5dd61
abaykann <azizakbaral@gmail.com>
2021-10-02 01:08:27 +0700

    Update [id].js

--- pages/products/edit/[id].js
+++ pages/products/edit/[id].js
@@ -1,106 +1,214 @@
-import React, { useState } from 'react';
-import { authPage } from '../../../middlewares/authorizationPage';
+import React, { Fragment } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ValidationSchema from '../../../components/validation/updateValidation';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import { styled } from '@mui/material/styles';
+import { useState } from "react";
+import Avatar from '@mui/material/Avatar';
 import Router from 'next/router';
-import Nav from '../../../components/Nav';
 
-export async function getServerSideProps(ctx) {
-    const { token } = await authPage(ctx);
-
-    const { id } = ctx.query;
-
-    const postReq = await fetch('http://localhost:3000/api/posts/detail/' + id, {
-        headers: {
-            'Authorization': 'Bearer ' + token
-        }
-    });
-
-    const res = await postReq.json();
-
+export const getStaticPaths = async () => {
+    const res = await fetch('http://localhost:3000/api/productsV2/');
+    const data = await res.json();
+  
+    // map data to an array of path objects with params (id)
+    const paths = data.data.map(product => {
+      return {
+        params: { id: product.id.toString() }
+      }
+    })
+  
     return {
-        props: {
-            token,
-            post: res.data
-        }
+      paths,
+      fallback: false
     }
-}
+  }
+  
+  export const getStaticProps = async (context) => {
+    const id = context.params.id;
+    const res = await fetch('http://localhost:3000/api/productsV2/detail/' + id);
 
-export default function PostEdit(props) {
-    const { post } = props;
+    const data = await res.json();
+  
+  
+    return {
+      props: { productData: data.data }
+    }
+  }
+  
+  const Details = ({ productData }) => {
+      console.log(productData)
 
-    const [ fields, setFields ] = useState({
-        title: post.title,
-        content: post.content
-    });
+      const {
+        register,
+        control,
+        handleSubmit,
+        formState: { errors }
+      } = useForm({
+        resolver: yupResolver(ValidationSchema)
+      });
 
-    const [ status, setStatus ] = useState('normal');
-
-    async function updateHandler(e) {
-        e.preventDefault();
+      const [image, setImage] = useState(productData.imageUrl);
+      const [createObjectURL, setCreateObjectURL] = useState(productData.imageUrl);
     
-        setStatus('loading');
+      const uploadToClient = (event) => {
+        if (event.target.files && event.target.files[0]) {
+          const i = event.target.files[0];
+    
+          setImage(i);
+          setCreateObjectURL(URL.createObjectURL(i));
+        }
+      };
 
-        const { token } = props;
+      
 
-        const update = await fetch('/api/posts/update/' + post.id, {
-            method: 'PUT',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + token
-            },
-            body: JSON.stringify(fields)
-        });
+      const Input = styled('input')({
+        display: 'none',
+      });
 
-        if(!update.ok) return setStatus('error');
 
-        const res = await update.json();
-
-        setStatus('success');
-
-        Router.push('/posts');
-    }
-
-    function fieldHandler(e) {
-        const name = e.target.getAttribute('name');
-       
-        setFields({
-            ...fields,
-            [name]: e.target.value
-        });
-    }
+      const onSubmit = async (data) => {
+        const formData = new FormData()
+        formData.append("file", data.picture[0])
+        formData.append("productName", data.productName)
+        formData.append("price", data.price)
+        formData.append("discount", data.discount)
+        formData.append("categoryId", data.categoryId)
+    
+        const res = await fetch('http://localhost:3000/api/productsV2/update/' + productData.id, {
+          method: "PUT",
+          body: formData
+        }).then(res => res.json())
+        // alert(JSON.stringify(res))
+        Router.push('/products');
+      }
 
     return (
-        <div>
-            <h1>Edit a Post</h1>
-
-            <Nav />
-        
-            <p>Post ID: {post.id}</p>
-
-            <form onSubmit={updateHandler.bind(this)}>
-               <input
-                    onChange={fieldHandler.bind(this)}
-                    type="text" 
-                    placeholder="Title" 
-                    name="title" 
-                    defaultValue={post.title}
+    <Fragment>
+        <Paper>
+          <Box px={3} py={2}>
+            <Typography variant="h6" align="center" margin="dense">
+              React Hook Form - Material UI - Validation
+            </Typography>
+  
+            <Grid container spacing={1}>
+              <Grid item xs={12} sm={12}>
+                <TextField
+                  required
+                  id="productName"
+                  name="productName"
+                  label="Product Name"
+                  fullWidth
+                  margin="dense"
+                  defaultValue={productData.productName}
+                  {...register('productName')}
+                  error={errors.productName ? true : false}
                 />
-                <br />
-                <textarea
-                    onChange={fieldHandler.bind(this)}
-                    placeholder="Content" 
-                    name="content" 
-                    defaultValue={post.content}
-                ></textarea>
-                <br />
-                
-                <button type="submit">
-                    Save Changes 
-                </button>
+                <Typography variant="inherit" color="textSecondary">
+                  {errors.productName?.message}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                   required
+                   id="price"
+                   name="price"
+                   label="price"
+                   defaultValue={productData.price}
+                   fullWidth
+                   margin="dense"
+                   {...register('price')}
+                   error={errors.price ? true : false}
+                 />
+                 <Typography variant="inherit" color="textSecondary">
+                   {errors.price?.message}
+                </Typography>
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                   required
+                   id="discount"
+                   name="discount"
+                   label="discount"
+                   fullWidth
+                   defaultValue={productData.discount}
+                   margin="dense"
+                   {...register('discount')}
+                   error={errors.discount ? true : false}
+                 />
+                 <Typography variant="inherit" color="textSecondary">
+                   {errors.discount?.message}
+                </Typography>
+              </Grid>
+              
+              <Grid item xs={12} sm={6}>
+              <FormControl fullWidth>
+            <InputLabel id="categoryId">Category Name</InputLabel>
+            <Select
+              id="categoryId"
+              name="categoryId"
+              label="categoryId"
+              fullWidth
+              defaultValue={productData.categoryId}
+              margin="dense"
+              {...register('categoryId')}
+              error={errors.categoryId ? true : false}>
+              <MenuItem value={1}>Bunga Papan</MenuItem>
+              <MenuItem value={2}>Bunga Meja</MenuItem>
+              <MenuItem value={3}>Hand Buket</MenuItem>
+              <MenuItem value={4}>Standing Flower</MenuItem>
+            </Select>
+            <Typography variant="inherit" color="textSecondary">
+              {errors.categoryId?.message}
+           </Typography>
+          </FormControl>
+         </Grid>
+         <Grid item xs={12} sm={6}> 
+        
 
-                <div>
-                    Status: {status}
-                </div>
-            </form>
-        </div>
+ <input {...register('picture')} type="file" name="picture" onChange={uploadToClient}/>
+      {errors.picture && <p>{errors.picture.message}</p>}
+
+
+</Grid>
+<Grid item xs={12} sm={6}> 
+
+<Avatar 
+  variant="square"
+  alt=""
+  src={createObjectURL}
+  sx={{ width: 600, height: 400 }}
+/>
+
+                {/* <img src={createObjectURL} /> */}
+</Grid>
+
+         </Grid>
+  
+            <Box mt={3}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleSubmit(onSubmit)}
+              >
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Paper>
+      </Fragment>
+
     );
-}
+  }
+  
+  export default Details;
