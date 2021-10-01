d5bbb6b380c8ba774a91be50701c766152121060
abaykann <azizakbaral@gmail.com>
2021-10-02 01:07:46 +0700

    update constants get env file

--- common/constants/constants.js
+++ common/constants/constants.js
@@ -1,12 +1,19 @@
-const constants = {};
+import * as dotenv from 'dotenv';
+dotenv.config({
+  path: process.env.ENV_FILE,
+  encoding: 'utf8',
+});
 
+const constants = {};
+constants.BASE_URL = process.env.BASE_URL || 'http://localhost:3000/';
 constants.HOST = 'localhost';
 constants.USER = 'root';
 constants.PASSWORD = '';
 constants.DB_NAME = 'anekabaru2'
-constants.JWT_SECRET = 'U8Nrx9r1Bsg1OXKZyaRlLIkhMr5thp5CQ3GXltyQht1v3wMtWi2LBWIIsXYdfXIPYPZt3thVp3IYnSb0W0BREaSkbWHBeFya70DC';
-constants.CLOUDINARY_CLOUD_NAME = 'akbaral';
-constants.CLOUDINARY_API_KEY = '578545818798377';
-constants.CLOUDINARY_API_SECRET = 'IgsQcB4ENJoAGpEDIyX8acUG0pc';
+constants.JWT_SECRET = process.env.JWT_SECRET || 'U8Nrx9r1Bsg1OXKZyaRlLIkhMr5thp5CQ3GXltyQht1v3wMtWi2LBWIIsXYdfXIPYPZt3thVp3IYnSb0W0BREaSkbWHBeFya70DC';
+constants.CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
+constants.CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY;
+constants.CLOUDINARY_API_SECRET = process.env.CLOUDINARY_API_SECRET;
+
 
 export default constants;
