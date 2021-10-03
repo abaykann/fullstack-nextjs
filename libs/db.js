0b08dd022eac3817dee2670a453a31f5f77774fd
abaykann <azizakbaral@gmail.com>
2021-10-04 00:36:37 +0700

    Update db.js

--- libs/db.js
+++ libs/db.js
@@ -10,19 +10,19 @@
 
 // export default knex;
 
-var knex = require('knex')({
-  client: 'pg',
-  connection: {
-      host: 'localhost',
-      user: 'alazizakbar',
-      password: '123456',
-      database: 'akbar',
-      port: 5432,
-      charset: 'utf8'
-  }
-});
+// var knex = require('knex')({
+//   client: 'pg',
+//   connection: {
+//       host: 'localhost',
+//       user: 'alazizakbar',
+//       password: '123456',
+//       database: 'akbar',
+//       port: 5432,
+//       charset: 'utf8'
+//   }
+// });
 
-export default knex;
+// export default knex;
 
 
 
@@ -40,16 +40,16 @@ export default knex;
 // export default knex;
 
 
-// var knex = require('knex')({
-//   client: 'pg',
-//   connection: {
-//     host: 'ec2-52-7-115-250.compute-1.amazonaws.com',
-//     user: 'axeerdtzavcghq',
-//     password: '784c58cd33acd9f420ba724b9f76b039ab49c14f89bfcf25b19cf30ef79f8899',
-//     database: 'dlgkmlei89533',
-//     ssl: { rejectUnauthorized: false }
-//   }
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    host: 'ec2-52-7-115-250.compute-1.amazonaws.com',
+    user: 'axeerdtzavcghq',
+    password: '784c58cd33acd9f420ba724b9f76b039ab49c14f89bfcf25b19cf30ef79f8899',
+    database: 'dlgkmlei89533',
+    ssl: { rejectUnauthorized: false }
+  }
   
-// });
+});
 
-// export default knex;
+export default knex;
