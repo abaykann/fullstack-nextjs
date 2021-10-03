3d64fd4dc8e7f9dc1461daa1aca87c092f8a786a
abaykann <azizakbaral@gmail.com>
2021-10-04 00:36:40 +0700

    Update login.js

--- pages/auth/login.js
+++ pages/auth/login.js
@@ -1,72 +1,79 @@
-import React, { useState, useEffect } from 'react';
-import Cookie from 'js-cookie';
-import Router from 'next/router';
-import { unauthPage } from '../../middlewares/authorizationPage';
-import Link from 'next/link';
+// import React, { useState, useEffect } from 'react';
+// import Cookie from 'js-cookie';
+// import Router from 'next/router';
+// import { unauthPage } from '../../middlewares/authorizationPage';
+// import Link from 'next/link';
 
-export async function getServerSideProps(ctx) {
-    await unauthPage(ctx);
+// export async function getServerSideProps(ctx) {
+//     await unauthPage(ctx);
 
-    return { props: {} }
-}
+//     return { props: {} }
+// }
 
-export default function Login() {
-    const [fields, setFields] = useState({
-        email: '',
-        password: ''
-    });
+// export default function Login() {
+//     const [fields, setFields] = useState({
+//         email: '',
+//         password: ''
+//     });
 
-    const [status, setStatus] = useState('normal');
+//     const [status, setStatus] = useState('normal');
 
-    async function loginHandler(e) {
-        e.preventDefault();
+//     async function loginHandler(e) {
+//         e.preventDefault();
         
-        setStatus('loading');
+//         setStatus('loading');
 
-        const loginReq = await fetch('/api/auth/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(fields)
-        });
+//         const loginReq = await fetch('/api/auth/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify(fields)
+//         });
 
-        if(!loginReq.ok) return setStatus('error ' + loginReq.status);
+//         if(!loginReq.ok) return setStatus('error ' + loginReq.status);
 
-        const loginRes = await loginReq.json();
+//         const loginRes = await loginReq.json();
 
-        setStatus('success');
+//         setStatus('success');
 
-        Cookie.set('token', loginRes.token);
+//         Cookie.set('token', loginRes.token);
 
-        Router.push('/posts');
-    }
+//         Router.push('/posts');
+//     }
 
-    function fieldHandler(e) {
-        const name = e.target.getAttribute('name');
+//     function fieldHandler(e) {
+//         const name = e.target.getAttribute('name');
 
-        setFields({
-            ...fields,
-            [name]: e.target.value
-        });
-    }
+//         setFields({
+//             ...fields,
+//             [name]: e.target.value
+//         });
+//     }
 
-    return (
-        <div>
-            <h1>Login</h1>
+//     return (
+//         <div>
+//             <h1>Login</h1>
             
-            <form onSubmit={loginHandler.bind(this)}>
-                <input onChange={fieldHandler.bind(this)} type="text" name="email" placeholder="Email" />
-                <input onChange={fieldHandler.bind(this)} type="password" name="password" placeholder="Password" />
+//             <form onSubmit={loginHandler.bind(this)}>
+//                 <input onChange={fieldHandler.bind(this)} type="text" name="email" placeholder="Email" />
+//                 <input onChange={fieldHandler.bind(this)} type="password" name="password" placeholder="Password" />
 
-                <button type="submit">
-                    Login
-                </button>
+//                 <button type="submit">
+//                     Login
+//                 </button>
 
-                <div>Status: {status}</div>
+//                 <div>Status: {status}</div>
 
-                <Link href="/auth/register"><a>Register</a></Link>
-            </form>
-        </div>
+//                 <Link href="/auth/register"><a>Register</a></Link>
+//             </form>
+//         </div>
+//     );
+// }
+
+
+export default function About() {
+    return (
+        <h1>Welcome</h1>
     );
-}
+  }
