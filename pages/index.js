37a37755e36f914fa8a4a71b7ab8e86c2fb5fff3
abaykann <azizakbaral@gmail.com>
2021-10-14 21:55:04 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
-export default function About() {
+import { authPage } from "../middlewares/authorizationPage";
+
+export async function getServerSideProps(ctx) {
+  const { token } = await authPage(ctx);
+  console.log(token)
+
+  return { 
+    props: {
+        token
+    }
+}
+
+}
+
+export default function Index(props) {
+  
+
   return (
-      <h1>Welcome</h1>
+      <div>
+        <h1>Ini index</h1>
+      </div>
+      
   );
 }
