44e4babf20da9d571bd88b148a4921e36e8c7620
abaykann <azizakbaral@gmail.com>
2021-10-14 21:54:47 +0700

    Update [id].js

--- pages/api/productsV2/delete/[id].js
+++ pages/api/productsV2/delete/[id].js
@@ -4,23 +4,8 @@ const cloudinary = require("../../../../common/config/cloudinaryConfig");
 export default async function handler(req, res) {
     if(req.method !== 'DELETE') return res.status(405).end();
 
-    // const auth = await authorization(req, res);
-
-    // const { id } = req.query;
-    // const data = await db('products').where({ id }).first();
-    // // JSON.parse(data)
-    // console.log(data)
-    // const result = await cloudinary.uploader.destroy(data.pubId);
-    // console.log(result)
-
-    // const deleteRow = await db('products').where({ id }).del();
-
-    // res.status(200);
-    // res.json({
-    //     message: 'Product deleted successfully'
-    // });
-
     try {
+        const auth = await authorization(req, res);
         const { id } = req.query;
         const data = await db('products').where({ id }).first();
         // JSON.parse(data)
